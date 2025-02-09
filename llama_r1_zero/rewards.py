eeb3afb957e51b8474add5ebd7ce3bf33beb84bb
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2025-02-09 20:23:58 +0800

    added similarity reward to address sparse rewards

--- llama_r1_zero/rewards.py
+++ llama_r1_zero/rewards.py
@@ -1,5 +1,8 @@
 import re
 from typing import List
+from sentence_transformers import SentenceTransformer, util
+
+embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
 def format_reward(completions, **kwargs) -> List[float]:
     """
@@ -35,3 +38,19 @@ def complexity_reward(completions, ground_truths, **kwargs) -> List[float]:
 
     max_length = max(reasoning_lengths) + 1e-5  # avoid division by zero
     return [length / max_length for length in reasoning_lengths]
+
+def similarity_reward(completions, ground_truths, **kwargs) -> List[float]:
+    """
+    Reward based on cosine similarity between the generated <answer> and the ground truth.
+    Uses sentence embeddings for semantic similarity.
+    """
+    pattern = r'<answer>(.*?)</answer>'
+    matches = [re.search(pattern, completion) for completion in completions]
+    answers = [match.group(1) if match else completions[i][-20:] for i,match in enumerate(matches)]
+
+    embeddings_completions = embedder.encode(answers, convert_to_tensor=True)
+    embeddings_ground_truths = embedder.encode(ground_truths, convert_to_tensor=True)
+
+    similarities = util.pytorch_cos_sim(embeddings_completions, embeddings_ground_truths).diagonal()
+
+    return similarities.tolist()
